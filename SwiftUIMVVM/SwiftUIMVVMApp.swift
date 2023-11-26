81874f2825c3f77d6629111360da471475a3fdd0
LucasGomeszx <lgomeszx@gmail.com>
2023-11-26 12:56:14 -0300

    Initial Commit

---
+++ SwiftUIMVVM/SwiftUIMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMVVMApp.swift
+//  SwiftUIMVVM
+//
+//  Created by Lucas Gomesx on 26/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
